af8a30be8a195e41edf658ca9f71d989ac4031e0
Kelvinu293 <157698488+Kelvinu293@users.noreply.github.com>
2024-09-23 16:46:59 +0100

    second start

--- week2-lab2/week2-lab2/App.js
+++ week2-lab2/week2-lab2/App.js
@@ -1,5 +1,16 @@
 import {View, Text} from 'react-native';
 
+const styles = StyleSheet.create({
+
+  
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
 export default function App(){
 
   return(
